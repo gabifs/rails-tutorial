2c64fd129d879d21983473c971b086554b450652
Gabriel Fernandes Silva <ga3riel.fernandess@gmail.com>
2021-01-13 12:53:43 -0300

    Bugfix

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,9 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticaded?
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 
